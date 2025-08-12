750d92299135acb97832f2b1d0fcd941a8da047d
Amirthavarshinih <156929622+Amirthavarshinih@users.noreply.github.com>
2025-08-12 11:13:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -227,4 +227,5 @@ def generate_reel():
     return jsonify({"video_url": video_url})
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0')
